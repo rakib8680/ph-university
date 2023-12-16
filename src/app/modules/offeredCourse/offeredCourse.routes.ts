675c85e398bee149757b210f681e804afb74858c
rakib8680 <rakib.cst.1st@gmail.com>
2023-12-16 20:49:27 +0600

    offered course validation with time validation implemented

--- src/app/modules/offeredCourse/offeredCourse.routes.ts
+++ src/app/modules/offeredCourse/offeredCourse.routes.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { OfferedCourseControllers } from './offeredCourse.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { OfferedCourseValidations } from './offeredCourse.validation';
 
 const router = express.Router();
 
 router.post(
   '/create-offered-course',
+  validateRequest(OfferedCourseValidations.createOfferedCourseValidationSchema),
   OfferedCourseControllers.createOfferedCourse,
 );
 
