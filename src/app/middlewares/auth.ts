d4f52868f28dcfdc3268e47d6a56096411a6e259
rakib8680 <rakib.cst.1st@gmail.com>
2023-12-20 17:56:11 +0600

    used more validation on auth middleware

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -4,33 +4,50 @@ import catchAsync from '../utils/catchAsync';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import { TUserRole } from '../modules/user/user.interface';
+import { User } from '../modules/user/user.model';
 
 // middleware for validating access token
 const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req, res, next) => {
     const token = req.headers.authorization;
-
     // if the token is sent from client
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized !');
     }
 
-    // verify the token
-    jwt.verify(token, config.jwt_access_secret as string, (err, decoded) => {
-      if (err) {
-        throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized !');
-      }
+    const decoded = jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+    ) as JwtPayload;
 
-      const role = (decoded as JwtPayload).role;
-      // if the user role is not matched
-      if (requiredRoles && !requiredRoles.includes(role)) {
-        throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized !');
-      }
+    const { role, userId, iat, exp } = decoded;
 
-      // set the user in req object
-      req.user = decoded as JwtPayload;
-      next();
-    });
+    // checking if the user is exist
+    const user = await User.isUserExistsByCustomId(userId);
+    if (!user) {
+      throw new AppError(httpStatus.NOT_FOUND, 'This user is not found !');
+    }
+
+    // checking if the user is already deleted
+    const isUserDeleted = user.isDeleted;
+    if (isUserDeleted) {
+      throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted !');
+    }
+
+    // checking if the user is blocked
+    const userStatus = user.status;
+    if (userStatus === 'blocked') {
+      throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked !');
+    }
+
+    // if the user role is not matched
+    if (requiredRoles && !requiredRoles.includes(role)) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized !');
+    }
+
+    // set the user in req object
+    req.user = decoded as JwtPayload;
+    next();
   });
 };
 
