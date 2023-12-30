f47d328cd5ffa0f544dd93a70ad47b596d00128f
rakib8680 <rakib.cst.1st@gmail.com>
2023-12-30 17:17:52 +0600

    grade calculator utility funcion created

--- src/app/modules/enrolledCourse/enrolledCourse.service.ts
+++ src/app/modules/enrolledCourse/enrolledCourse.service.ts
@@ -202,19 +202,31 @@ const updateEnrolledCourseMarksIntoDB = async (
     ...courseMarks,
   };
 
-  if (courseMarks && Object.keys(courseMarks).length) {
-    for (const [key, value] of Object.entries(courseMarks)) {
-      modifiedData[`courseMarks.${key}`] = value;
+  if (courseMarks?.finalTerm) {
+    const { classTest1, classTest2, midTerm, finalTerm } =
+      isCourseBelongToFaculty.courseMarks;
+
+    const totalMarks =
+      Math.ceil(classTest1 * 0.1) +
+      Math.ceil(midTerm * 0.3) +
+      Math.ceil(classTest2 * 0.1) +
+      Math.ceil(finalTerm * 0.5);
+
+
+    if (courseMarks && Object.keys(courseMarks).length) {
+      for (const [key, value] of Object.entries(courseMarks)) {
+        modifiedData[`courseMarks.${key}`] = value;
+      }
     }
+
+    // const result = await EnrolledCourse.findOneAndUpdate(
+    //   isCourseBelongToFaculty._id,
+    //   modifiedData,
+    //   { new: true },
+    // );
+
+    // return result;
   }
-
-  const result = await EnrolledCourse.findOneAndUpdate(
-    isCourseBelongToFaculty._id,
-    modifiedData,
-    { new: true },
-  );
-
-  return result;
 };
 
 export const EnrolledCourseServices = {
