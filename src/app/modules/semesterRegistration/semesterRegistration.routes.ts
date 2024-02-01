f9ac6ef1999245db3235b5adf7358f8ce448b317
rakib8680 <rakib.cst.1st@gmail.com>
2024-02-01 13:14:24 +0600

    Add authentication middleware to semester registration routes

--- src/app/modules/semesterRegistration/semesterRegistration.routes.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.routes.ts
@@ -2,18 +2,36 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { SemesterRegistrationValidations } from './semesterRegistration.validation';
 import { SemesterRegistrationController } from './semesterRegistration.controller';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
-router.get('/', SemesterRegistrationController.getAllSemesterRegistrations);
+router.get(
+  '/',
+  auth(
+    USER_ROLE.admin,
+    USER_ROLE.superAdmin,
+    USER_ROLE.student,
+    USER_ROLE.faculty,
+  ),
+  SemesterRegistrationController.getAllSemesterRegistrations,
+);
 
 router.get(
   '/:id',
+  auth(
+    USER_ROLE.admin,
+    USER_ROLE.superAdmin,
+    USER_ROLE.student,
+    USER_ROLE.faculty,
+  ),
   SemesterRegistrationController.getSingleSemesterRegistration,
 );
 
 router.post(
   '/create-semester-registration',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequest(
     SemesterRegistrationValidations.createSemesterRegistrationValidationSchema,
   ),
@@ -22,6 +40,7 @@ router.post(
 
 router.patch(
   '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequest(
     SemesterRegistrationValidations.updateSemesterRegistrationValidationSchema,
   ),
@@ -29,6 +48,7 @@ router.patch(
 );
 router.delete(
   '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   SemesterRegistrationController.deleteSemesterRegistration,
 );
 
