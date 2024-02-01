c6bd69a0394d00d4295db66a8f67d6e16db42f7b
rakib8680 <rakib.cst.1st@gmail.com>
2024-02-01 13:12:44 +0600

    Add authentication middleware to academic semester routes

--- src/app/modules/academicSemester/academic.Sem.Route.ts
+++ src/app/modules/academicSemester/academic.Sem.Route.ts
@@ -5,18 +5,41 @@ import {
   createAcademicSemValidationSchema,
   updateAcademicSemValidationSchema,
 } from './academic.Sem.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
-router.get('/', academicSemesterControllers.getAllAcademicSemester);
+router.get(
+  '/',
+  auth(
+    USER_ROLE.admin,
+    USER_ROLE.superAdmin,
+    USER_ROLE.student,
+    USER_ROLE.faculty,
+  ),
+  academicSemesterControllers.getAllAcademicSemester,
+);
 router.post(
   '/create-academic-semester',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequest(createAcademicSemValidationSchema),
   academicSemesterControllers.createAcademicSemester,
 );
-router.get('/:semesterId', academicSemesterControllers.getSingleSemester);
+router.get(
+  '/:semesterId',
+  auth(
+    USER_ROLE.admin,
+    USER_ROLE.superAdmin,
+    USER_ROLE.student,
+    USER_ROLE.faculty,
+  ),
+  academicSemesterControllers.getSingleSemester,
+);
+
 router.patch(
   '/:semesterId',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequest(updateAcademicSemValidationSchema),
   academicSemesterControllers.updateAcademicSemester,
 );
